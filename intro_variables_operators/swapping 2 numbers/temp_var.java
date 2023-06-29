c61f8fdb6dddb33b182e75b457830a0914fe3bc4
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-06-29 22:26:56 +0530

    Create temp_var.java

---
+++ intro_variables_operators/swapping 2 numbers/temp_var.java
@@ -0,0 +1,12 @@
+class sawp {
+    public static void main(String[] args) 
+    {
+        int a=1;
+        int b=5;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
