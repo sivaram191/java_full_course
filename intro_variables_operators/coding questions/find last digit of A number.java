4ece6d8cc56f04d822893743013b7aead0f5b54f
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-06-30 22:13:03 +0530

    Create find last digit of A number.java

---
+++ intro_variables_operators/coding questions/find last digit of A number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("enter number");
+		Scanner s = new Scanner(System.in);
+		int A=s.nextInt();
+		int ans = A%10;
+		System.out.println(ans);
+	}
+}
