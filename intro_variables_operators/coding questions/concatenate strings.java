4efb27b16bccb028f2d3d343d6c4c84f02b2fb97
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-06-30 22:29:23 +0530

    Create concatenate strings.java

---
+++ intro_variables_operators/coding questions/concatenate strings.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.Math;
+public class Main
+{
+	public static void main(String[] args) {
+	    String firstName = "John";
+        String lastName = "Doe";
+        String fullName1 = firstName + " " + lastName;
+        String fullName2 = firstName.concat(" ").concat(lastName);
+        System.out.println(fullName1);
+        System.out.println(fullName2);
+	
+	}
+}
