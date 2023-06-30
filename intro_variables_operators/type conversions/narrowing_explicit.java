09f691da0b05decf1d15123fc94ff9f53aa398fb
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-06-30 10:46:45 +0530

    Create narrowing_explicit.java

---
+++ intro_variables_operators/type conversions/narrowing_explicit.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    double a = 100.23;
+	    long b = (long)a;
+	    float c = (float)a;
+	    int d = (int)a;
+	    System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+	}
+}
+
