83b9945c58eeba5f04727e90cd01343c5c4036e3
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-01 21:15:57 +0530

    Create continue.java

---
+++ Flow control_loops_functions/continue.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    for(int a = 1; a <= 10; a++)
+	    {
+	        if(a == 4)
+	        {
+	            continue;
+	        }
+	        System.out.println(a);
+	    }
+	    	
+	}
+}
