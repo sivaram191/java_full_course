442fa196df34aa5b0a481048a83cdb1d3bf916f4
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-01 21:13:47 +0530

    Create break.java

---
+++ Flow control_loops_functions/break.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    for(int a = 1; a <= 10; a++)
+	    {
+	        System.out.println(a);
+	        if(a == 4)
+	        {
+	            break;
+	        }
+	        
+	    }
+	    	
+	}
+}
