76d49b7e0c76a657af3dc5ef7d8ac7e3ab05d089
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-01 21:58:02 +0530

    Create reverse_half_paramid.java

---
+++ Flow control_loops_functions/patterns/reverse_half_paramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner s = new Scanner(System.in);
+	    System.out.println("enter no of rows");
+	    int rows = s.nextInt();
+	    
+	    for(int i=rows;i>=1;i--)
+	    {
+	        for(int j=1;j<=i;j++)
+	        {
+	            System.out.print("* ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
