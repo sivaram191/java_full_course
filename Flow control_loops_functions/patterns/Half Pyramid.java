6fd47fc91fab356f3536124187207bca89debdf8
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-01 21:57:12 +0530

    Update Half Pyramid.java

--- Flow control_loops_functions/patterns/Half Pyramid.java
+++ Flow control_loops_functions/patterns/Half Pyramid.java
@@ -6,7 +6,6 @@ public class Main
 	    Scanner s = new Scanner(System.in);
 	    System.out.println("enter no of rows");
 	    int rows = s.nextInt();
-	    int ans;
 	    for(int i=1;i<=rows;i++)
 	    {
 	        for(int j=1;j<=i;j++)
