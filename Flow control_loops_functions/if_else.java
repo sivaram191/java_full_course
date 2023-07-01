c35fbf24af1f6300ad870629bf6f84eddc9f558b
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-01 11:36:27 +0530

    Create if_else.java

---
+++ Flow control_loops_functions/if_else.java
@@ -0,0 +1,16 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a = 48;
+	    double b = 78.44;
+	    if(a>b)
+	    {
+	       System.out.println("a is larger"); 
+	    }
+	    else
+	    {
+	       System.out.println("b is larger");
+	    }
+		
+	}
+}
