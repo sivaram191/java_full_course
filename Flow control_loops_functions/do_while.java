6953e8aa995468532acf0be6b0108eaf983a2104
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-01 21:06:30 +0530

    Create do_while.java

---
+++ Flow control_loops_functions/do_while.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int a = 1;
+	    
+	    do
+	    {
+	        System.out.println(a);
+	        a++;
+	    }
+	    while(a<=5);
+	}
+}
