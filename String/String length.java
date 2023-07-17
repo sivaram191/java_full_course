1944662ab4f31583680b11e8a0bd0233787b051c
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-17 15:54:34 +0530

    Create String length.java

---
+++ String/String length.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1 = "Hello";
+        String str2 = new String("World");
+        String str3 = "Hello 1";
+		System.out.println(str1.length());
+		System.out.println(str2.length());
+		System.out.println(str3.length());
+	}
+}
