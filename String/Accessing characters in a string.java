91e2217c7e18d04473ac7ec751c2c867e14427d7
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-17 15:57:35 +0530

    Create Accessing characters in a string.java

---
+++ String/Accessing characters in a string.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1 = "Hello";
+        String str2 = new String("World");
+        String str3 = "Hello 1";
+		System.out.println(str1.charAt(1));
+		System.out.println(str2.charAt(3));
+		System.out.println(str3.charAt(2));
+	}
+}
