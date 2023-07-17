7cf026c5879442b1c077e6561f069d9233830895
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-17 15:42:56 +0530

    Create String declaration and initialization.java

---
+++ String/String declaration and initialization.java
@@ -0,0 +1,22 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1 = "Hello";
+
+        // Using the new keyword
+        String str2 = new String("World");
+        
+        // Empty string
+        String str3 = "";
+        
+        // Null string
+        String str4 = null;
+        
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+		System.out.println(str4);
+		
+	}
+}
