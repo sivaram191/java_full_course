e8e4d9013f041292155cd787ad2b2fd381a3c573
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-17 21:55:18 +0530

    Create String parsing.java

---
+++ String/String parsing.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+    String str = "100";
+    int num = Integer.parseInt (str);
+    double dou = Double.parseDouble (str);
+    float flo = Float.parseFloat (str);
+    long lng = Long.parseLong (str);
+      System.out.println (num);
+      System.out.println (dou);
+      System.out.println(flo);
+      System.out.println(lng);
+
+  }
+}
