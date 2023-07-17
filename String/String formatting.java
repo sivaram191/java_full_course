5c6cce7a12399ba5e6b90aae641a961979a154f3
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-17 21:40:35 +0530

    Create String formatting.java

---
+++ String/String formatting.java
@@ -0,0 +1,10 @@
+import java.util.regex.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "john";
+        int age = 20;
+        double marks = 59.09;
+        System.out.printf("%s is %d years old, with %.2f marks in his test",name,age,marks);
+    }
+}
