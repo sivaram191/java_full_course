e2e5c278719971b423967754c64e7e9a9ff6278c
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-17 15:46:42 +0530

    Create String concatenation.java

---
+++ String/String concatenation.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1 = "Hello";
+      String str2 = new String("World");
+  		System.out.println(str1+" "+str2);
+  		System.out.println(str1.concat(" ").concat(str2));
+	}
+}
