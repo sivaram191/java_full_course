58634e55d74fd0bf119f058bfdb90caff7252910
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2023-07-17 16:01:06 +0530

    Create Modifying strings.java

---
+++ String/Modifying strings.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String str1 = "Hello";
+        String str2 = str1.substring(3,4);
+        String str3 = str1.replace("l","a");
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+	}
+}
